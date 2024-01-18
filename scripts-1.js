5ed42e4c9c3928f2e0ec7639e8d90b97864e0b04
ingsporttv <129752570+ingtv@users.noreply.github.com>
2024-01-18 13:18:14 +0300

    Update scripts-1.js

--- scripts-1.js
+++ scripts-1.js
@@ -1,10 +1,10 @@
 function go(url) {
-    if (url.startsWith("go:playm")) {
-        // 'go:' ile baÅŸlayan URL'leri iÅŸle
-        var target = url.slice(3); // 'go:' Ã¶nekini kaldÄ±r
+    if (url.startsWith("go:playtv01")) {
+        // 'go:' ile baÃ…Å¸layan URL'leri iÃ…Å¸le
+        var target = url.slice(3); // 'go:' ÃƒÂ¶nekini kaldÃ„Â±r
 
-        // Yeni sayfaya gitmek iÃ§in kullanÄ±lacak kodu ekleyin
-        // Ã–rneÄŸin, yeni sayfaya yÃ¶nlendirme yapmak iÃ§in:
+        // Yeni sayfaya gitmek iÃƒÂ§in kullanÃ„Â±lacak kodu ekleyin
+        // Ãƒâ€“rneÃ„Å¸in, yeni sayfaya yÃƒÂ¶nlendirme yapmak iÃƒÂ§in:
         // window.location.href = target;
     }
 }
@@ -28,27 +28,27 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let i = 0; i < kanalKutulari.length; i++) {
         kanalKutulari[i].addEventListener("click", function () {
             const link = this.getAttribute("href");
-            go(link); // TÄ±klanan kanalÄ±n baÄŸlantÄ±sÄ±nÄ± 'go' fonksiyonuna ileterek iÅŸle
+            go(link); // TÃ„Â±klanan kanalÃ„Â±n baÃ„Å¸lantÃ„Â±sÃ„Â±nÃ„Â± 'go' fonksiyonuna ileterek iÃ…Å¸le
         });
     }
 
-    // Ä°zin verilen User-Agent'i belirtin
-    var allowedUserAgent = "playm";
+    // Ã„Â°zin verilen User-Agent'i belirtin
+    var allowedUserAgent = "play01";
 
-    // KullanÄ±cÄ± ajanÄ±nÄ± al
+    // KullanÃ„Â±cÃ„Â± ajanÃ„Â±nÃ„Â± al
     var userAgent = navigator.userAgent.toLowerCase();
 
-    // KullanÄ±cÄ± ajanÄ±nÄ± kontrol edin ve izin verilen User-Agent'e sahip olup olmadÄ±ÄŸÄ±nÄ±zÄ± kontrol edin
+    // KullanÃ„Â±cÃ„Â± ajanÃ„Â±nÃ„Â± kontrol edin ve izin verilen User-Agent'e sahip olup olmadÃ„Â±Ã„Å¸Ã„Â±nÃ„Â±zÃ„Â± kontrol edin
     if (!userAgent.includes(allowedUserAgent)) {
-        // Ä°zin verilen User-Agent'e sahip olmayan kullanÄ±cÄ±lar iÃ§in sayfa iÃ§eriÄŸini gizle
+        // Ã„Â°zin verilen User-Agent'e sahip olmayan kullanÃ„Â±cÃ„Â±lar iÃƒÂ§in sayfa iÃƒÂ§eriÃ„Å¸ini gizle
         var containers = document.querySelectorAll(".kanal-kutusu");
         for (var i = 0; i < containers.length; i++) {
             containers[i].style.display = "none";
         }
 
-        // Ä°zin verilen User-Agent'e sahip olmayan kullanÄ±cÄ±lar iÃ§in bir hata mesajÄ± gÃ¶ster
+        // Ã„Â°zin verilen User-Agent'e sahip olmayan kullanÃ„Â±cÃ„Â±lar iÃƒÂ§in bir hata mesajÃ„Â± gÃƒÂ¶ster
         var errorMessage = document.createElement("div");
-        errorMessage.textContent = "Üzgünüz, internet bağlantınzı kontrol edin. Sayfa yüklenemiyor.";
+        errorMessage.textContent = "ÃœzgÃ¼nÃ¼z, internet baÄŸlantÄ±nzÄ± kontrol edin. Sayfa yÃ¼klenemiyor.";
         errorMessage.style.fontSize = "24px";
         errorMessage.style.color = "red";
         errorMessage.style.textAlign = "center";
